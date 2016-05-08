05c6d5d41bca023f30ba43710cea85552616afe5
sam-rez <srezaie@calpoly.edu>
2016-05-07 22:32:45 -0700

    Added star wars font and list_view class and xml

---
+++ app/src/main/java/com/example/android/starwarsv2/ListView.java
@@ -0,0 +1,11 @@
+package com.example.android.starwarsv2;
+
+/**
+ * Created by Sam on 5/7/16.
+ */
+
+public class ListView {
+
+
+
+}
--- app/src/main/java/com/example/android/starwarsv2/MainActivity.java
+++ app/src/main/java/com/example/android/starwarsv2/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.android.starwarsv2;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.swapi.models.Film;
+import com.swapi.models.People;
 import com.swapi.models.Planet;
 import com.swapi.models.SWModelList;
 import com.swapi.sw.StarWarsApi;
@@ -22,22 +25,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        TextView textViewCustom = (TextView) findViewById(R.id.test);
+        Typeface myCustomFont = Typeface.createFromAsset(getAssets(), "fonts/Starjedi.ttf");
+        textViewCustom.setTypeface(myCustomFont);
+
         StarWarsApi.init();
 
-        StarWarsApi.getApi().getAllPlanets(2, new Callback<SWModelList<Planet>>() {
-            @Override
-            public void success(SWModelList<Planet> planetSWModelList, Response response) {
-                Log.e("TEST", "TESTING");
-                System.out.print(planetSWModelList);
+        for(int i = 1; i <= 7; i++) {
+            StarWarsApi.getApi().getAllPeople(i, new Callback<SWModelList<People>>() {
+                @Override
+                public void success(SWModelList<People> planetSWModelList, Response response) {
+                    Log.e("TEST", "TESTING");
+                    for (People p : planetSWModelList.results) {
+                        System.out.println("Person:" + p.name);
+                    }
 
-            }
+                }
 
-            @Override
-            public void failure(RetrofitError error) {
-                System.out.print("failure");
-            }
-        });
-
+                @Override
+                public void failure(RetrofitError error) {
+                    System.out.print("failure");
+                }
+            });
+        }
 
     }
 
